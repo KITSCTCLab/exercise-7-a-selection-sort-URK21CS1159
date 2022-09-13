4f97167e543f491c56c871459d5e9407fd042acb
URK21CS1159 <110010698+URK21CS1159@users.noreply.github.com>
2022-09-13 10:57:41 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,14 +7,12 @@ def selectionSort(array, size) -> List[int]:
 
         for i in range(step + 1, size):
          
-
             if array[i] < array[min_idx]:
                 min_idx = i
-         
-        # put min at the correct position
+        
         (array[step], array[min_idx]) = (array[min_idx], array[step])
 
-  
+  return array
 
 # Do not change the following code
 input_data = input()
